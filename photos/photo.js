150f2ef84aef270e09f3b6384dd4dcb8fa23a8ac
mjntutanota <mjn1234@tutanota.com>
2019-06-25 16:02:39 +0800

    Site updated: 2019-06-25 16:02:39

--- photos/photo.js
+++ photos/photo.js
@@ -1,93 +1,46 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="\assets\js\Meting.min.js"></script>photo ={
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="\assets\js\Meting.min.js"></script>define([], function () {
+        return {
+            page: 1,
+            offset: 20,
+            init: function () {
+                var that = this;
+                $.getJSON("/photo/output.json", function (data) {
+                    that.render(that.page, data);
 
-    page: 1,
+                    that.scroll(data);
+                });
+            },
 
-    offset: 20,
+            render: function (page, data) {
+                var begin = (page - 1) * this.offset;
+                var end = page * this.offset;
+                if (begin >= data.length) return;
+                var html, li = "";
+                for (var i = begin; i < end && i < data.length; i++) {
+                    li += '<li><div class="img-box">' +
+                        '<a class="img-bg" rel="noopener" href="https://github.com/lwzhang/blog/blob/master/photos/' + data[i] + '?raw=true" target="_blank"></a>' +
+                        '<img lazy-src="https://github.com/lwzhang/blog/blob/master/photos/' + data[i] + '?raw=true">' +
+                        '</div></li>';
+                }
 
-    init: function () {
+                $(".img-box-ul").append(li);
+                $(".img-box-ul").lazyload();
+                $("a[rel=example_group]").fancybox();
+            },
 
-        var that = this;
+            scroll: function (data) {
+                var that = this;
+                $(window).scroll(function() {
+                    var windowPageYOffset = window.pageYOffset;
+                    var windowPageYOffsetAddHeight = windowPageYOffset + window.innerHeight;
+                    var sensitivity = 0;
 
-        $.getJSON("/photos/photoslist.json", function (data) {
+                    var offsetTop = $(".instagram").offset().top + $(".instagram").height();
 
-            that.render(that.page, data);
-
-            //that.scroll(data);
-
-        });
-
-    },
-
-    render: function (page, data) {
-
-        var begin = (page - 1) * this.offset;
-
-        var end = page * this.offset;
-
-        if (begin >= data.length) return;
-
-        var html, imgNameWithPattern, imgName, imageSize, imageX, imageY, li = "";
-
-        for (var i = begin; i < end && i < data.length; i++) {
-
-           imgNameWithPattern = data[i].split(' ')[1];
-
-           imgName = imgNameWithPattern.split('.')[0]
-
-           imageSize = data[i].split(' ')[0];
-
-           imageX = imageSize.split('.')[0];
-
-           imageY = imageSize.split('.')[1];
-
-            li += '<div class="card" style="width:330px">' +
-
-                    '<div class="ImageInCard" style="height:'+ 330 * imageY / imageX + 'px">' +
-
-                      '<a data-fancybox="gallery" href="https://github.com/asdfv1929/BlogPhotos/blob/master/Images/' + imgNameWithPattern + '?raw=true" data-caption="' + imgName + '" target="_blank" rel="noopener">' +
-
-                        '<img src="https://github.com/asdfv1929/BlogPhotos/blob/master/Images/' + imgNameWithPattern + '?raw=true">' +
-
-                      '</a>' +
-
-                    '</div>' +
-
-                    // '<div class="TextInCard">' + imgName + '</div>' +
-
-                  '</div>'
-
+                    if (offsetTop >= windowPageYOffset && offsetTop < windowPageYOffsetAddHeight + sensitivity) {
+                        that.render(++that.page, data);
+                    }
+                })
+            }
         }
-
-        $(".ImageGrid").append(li);
-
-        $(".ImageGrid").lazyload();
-
-        this.minigrid();
-
-    },
-
-    minigrid: function() {
-
-        var grid = new Minigrid({
-
-            container: '.ImageGrid',
-
-            item: '.card',
-
-            gutter: 12
-
-        });
-
-        grid.mount();
-
-        $(window).resize(function() {
-
-           grid.mount();
-
-        });
-
-    }
-
-}
-
-photo.init();
+    })
